ea90d7f2ea507621f0df1d2b91b2ec6f47909aa8
dfdavid <fd.dandrea@unc.edu.ar>
2023-12-26 14:16:18 -0300

    render template

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,18 @@
-print('hello world')
+from flask import Flask, render_template
+
+# Crea una instancia de la aplicación Flask
+app = Flask(__name__)
+
+# Define una ruta (endpoint) para la página principal
+@app.route('/')
+def index():
+    return '¡Bienvenido a mi server!'
+
+@app.route('/sa')
+def spurs():
+    return render_template('spurs.html')
+
+# Ejecuta la aplicación si este archivo es el archivo principal
+if __name__ == '__main__':
+    app.run(debug=True)
+
